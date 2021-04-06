d5e3631c72f7809b6892b6c16fa83cdce2c85389
Matīss Petrausks <matiss44555@gmail.com>
2021-04-06 11:43:44 +0300

    Uzsākts pirmais skripts/pievienoti atteli, skanas, utt

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jāimportē, lai varētu strādāt ar EventSystem
+using UnityEngine.EventSystems;
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler/*, IEndDragHandler*/{
+    //Uzglabāt norādi uz Objektu skriptu
+    //public Objekti objektuSkripts;
+    //Velkamam objektam piestiprinātā komponente
+    private CanvasGroup kanvasGrupa;
+    //Vilktā objekta atrašanās vietas koordinātu maiņai
+    private RectTransform velkObjRectTransf;
+
+    void Awake()
+    {
+        //Piekļūst objektam piesaistītajai CanvasGroup komponentei
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        //Piekļūst objekta RectTransform komponentei
+        velkObjRectTransf = GetComponent<RectTransform>();
+    }
+
+    //Nostrādā nospiežot peles klikšķi uz objekta
+    public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Uzklikšķināts uz velkamā objekta!");
+    }
+
+    //Nostrādā uzsākot vilkšanu
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        //Attīra pēdējo vilkto objektu
+        //objektuSkripts.pedejaisVilktais = null;
+        Debug.Log("Uzsākta vilkšana!");
+        //Uzsākot vilkt objektu tas paliek nedaudz caurspīdīgs
+        kanvasGrupa.alpha = 0.6f;
+        //Lai objektam varētu iet cauri raycast stars
+        kanvasGrupa.blocksRaycasts = false;
+    }
+    //Nostrādā reāli notiekot vilkšanai
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notiek vilkšana!");
+        //velkObjRectTransf.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+    }
+}
