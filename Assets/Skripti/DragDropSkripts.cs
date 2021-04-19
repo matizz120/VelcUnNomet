c5e94c2f65fc8bee835e454a5d3144bc73cb987b
Matīss Petrausks <matiss44555@gmail.com>
2021-04-19 13:37:29 +0300

    Pārlaboti faili

--- Assets/Skripti/DragDropSkripts.cs
+++ Assets/Skripti/DragDropSkripts.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-//Jāimportē, lai varētu strādāt ar EventSystem
+//Jāimportē, lai varētu strādāt ar EventSystems
 using UnityEngine.EventSystems;
-public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler{
-    //Uzglabāt norādi uz Objektu skriptu
+
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    //Uzglabās norādi uz Objektu skriptu
     public Objekti objektuSkripts;
     //Velkamam objektam piestiprinātā komponente
     private CanvasGroup kanvasGrupa;
     //Vilktā objekta atrašanās vietas koordinātu maiņai
     private RectTransform velkObjRectTransf;
-
+    
     void Awake()
     {
         //Piekļūst objektam piesaistītajai CanvasGroup komponentei
@@ -29,36 +29,48 @@ public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHan
     //Nostrādā uzsākot vilkšanu
     public void OnBeginDrag(PointerEventData notikums)
     {
+        Debug.Log("Uzsākta vilkšana!");
         //Attīra pēdējo vilkto objektu
         objektuSkripts.pedejaisVilktais = null;
-        Debug.Log("Uzsākta vilkšana!");
         //Uzsākot vilkt objektu tas paliek nedaudz caurspīdīgs
         kanvasGrupa.alpha = 0.6f;
         //Lai objektam varētu iet cauri raycast stars
         kanvasGrupa.blocksRaycasts = false;
     }
+
     //Nostrādā reāli notiekot vilkšanai
     public void OnDrag(PointerEventData notikums)
     {
         Debug.Log("Notiek vilkšana!");
         velkObjRectTransf.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
     }
-    public void OnEndDrag(PointerEventData notikums){
+
+    //Nostrādā, kad tiek beigta vilkšana
+    public void OnEndDrag(PointerEventData notikums)
+    {
+        //Atlaižot objektu iestata to kā pēdējo vilkto
         objektuSkripts.pedejaisVilktais = notikums.pointerDrag;
         Debug.Log("Pēdējais vilktais objekts: "+objektuSkripts.pedejaisVilktais);
-         Debug.Log("Beidzas!");
-         //Atlaižot objektu paliek necaurspīdīgs
-         kanvasGrupa.alpha = 1f;
+        Debug.Log("Beigta vilkšana!");
+        //Atlaižot objektu tas atkal paliek necaurspīdīgs
+        kanvasGrupa.alpha = 1f;
 
-         //Pārbauda vai objekts ir vienkārši nomest vai arī nomests pareizajā vietā
-         if(objektuSkripts.vaiIstajaVieta == false){
-            //Ja nav nomests pareizajā vietā, atkal padara velkamu
+        //Pārbauda vai objekts ir vienkārši nomest vai arī nomests pareizajā vietā
+        if (objektuSkripts.vaiIstajaVieta == false)
+        {
+            //ja nav nomests pareizajā vietā, tad atkal padara velkamu
             kanvasGrupa.blocksRaycasts = true;
-         }else{
-             //Ja objekts nolikts pareizajā vietā, izmērā, rotācijā , tad pēdējo vilkto attīra
-             objektuSkripts.pedejaisVilktais = null;
-         }
-         // Ja viens objekts nomests pareizajā vietā, tad lai varētu tuorpināt pārvietot pārējos objektus
-         objektuSkripts.vaiIstajaVieta = false;
+
+        }
+        else
+        {
+            /*Ja ojbkets nolikts pareizajā vietā, izmērā, rotācijā, tad
+            pēdējo vilkto attīra*/
+            objektuSkripts.pedejaisVilktais = null;
+        }
+
+        /*Ja viens objekts nomests pareizajā vietā, tad lai varētu turpināt
+        pārvietot pārējos objektus*/
+        objektuSkripts.vaiIstajaVieta = false;
     }
 }
--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class Objekti : MonoBehaviour
 {
     //GameObject, kas uzglabā visus velkamos objektus
-    public GameObject autobuss;
-    public GameObject atroMasina;
     public GameObject atkritumuMasina;
-    //Uzglabās velkamo objektu sākotnējās pazīcijas koordinātas (lai zinātu, kur aizmest objektu, ja tas nolikts nepareizajā vietā)
+    public GameObject atroMasina;
+    public GameObject autobuss;
+
+    /*Uzglabās velkamo objektu sākotnējās pozīcijas
+    koordinātas (lai zinātu, kur aizmest objektu, ja tas nolikts nepareizajā vietā)*/
     //Objekti paliek Public, taču paslēpj no Inspector loga
     [HideInInspector]
     public Vector2 atkrKoord;
@@ -20,13 +22,15 @@ public class Objekti : MonoBehaviour
     public Canvas kanva;
     //Uzglabās skaņas avotu, kurā atskaņot audio failu
     public AudioSource skanasAvots;
-    //Masivs, kas uzglabā audio failus
+    //Masīvs, kas uzglabās visas skaņas
     public AudioClip[] skanaKoAtskanot;
     //Uzglabās objektu, kurš ir pēdējais pārvietotais
     public GameObject pedejaisVilktais = null;
     //Mainīgais atbild par to vai objekts ir nolikts pareizi vai nepareizi
     [HideInInspector]
     public bool vaiIstajaVieta = false;
+
+    //Funkcija nostrādā tiklīdz nospiesta play poga
     private void Awake()
     {
         atkrKoord = atkritumuMasina.GetComponent<RectTransform>().localPosition;
